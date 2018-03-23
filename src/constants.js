c3faa12672de9b0c07b672fe50d52571a51eced1
sixertoy <sixertoy.github@gmail.com>
2018-03-23 19:29:20 +0100

    🍿  add reducers and some constants

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,9 @@
 const FORM_NAME = 'decisionnal';
+const FORM_RESET = 'onFormReset';
+const FORM_LOADED = 'onFormLoaded';
 
 export default {
   FORM_NAME,
+  FORM_RESET,
+  FORM_LOADED,
 };
---
+++ src/reducers/stepper.js
@@ -0,0 +1,29 @@
+export const steps = (state = {}, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export const disabled = (state = {}, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export const fields = (state = {}, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export const current = (state = {}, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default steps;
