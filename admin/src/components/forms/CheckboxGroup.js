43e3e7e0d72c8cd19473bae458c69a47a728461d
sixertoy <sixertoy.github@gmail.com>
2018-04-17 20:00:43 +0200

    🚧  [ADMIN] add radiogroup et checkboxgroup form inputs

--- admin/src/components/forms/CheckboxGroup.js
+++ admin/src/components/forms/CheckboxGroup.js
@@ -2,9 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Field } from 'react-final-form';
 
-const getkey = (name, index) => `radio::${name}::${index}`;
+const getkey = (name, index) => `checkbox::${name}::${index}`;
 
-const RadioGroup = ({
+const CheckboxGroup = ({
   name, label, provider, display, ...rest
 }) => {
   const cssclass = `flex-${display !== 'inline' ? 'rows' : 'columns'}`;
@@ -16,7 +16,11 @@ const RadioGroup = ({
           {provider &&
             provider.map(obj => (
               <span key={getkey(name, obj.id)}>
-                <Field {...rest} name={name} type="radio" component="input" />
+                <Field {...rest}
+                  name={name}
+                  value={obj.id}
+                  type="checkbox"
+                  component="input" />
                 <span>{obj.name}</span>
               </span>
             ))}
@@ -26,11 +30,11 @@ const RadioGroup = ({
   );
 };
 
-RadioGroup.propTypes = {
+CheckboxGroup.propTypes = {
   name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   display: PropTypes.string.isRequired,
   provider: PropTypes.array.isRequired,
 };
 
-export default RadioGroup;
+export default CheckboxGroup;
