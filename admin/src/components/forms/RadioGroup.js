a8fbbb5e743c692330675cc0cb7b038d6cb247d9
sixertoy <sixertoy.github@gmail.com>
2018-04-17 19:09:35 +0200

    🚧  [ADMIN] modification du display des SUOS dans la page restrictions

--- admin/src/components/forms/RadioGroup.js
+++ admin/src/components/forms/RadioGroup.js
@@ -5,25 +5,31 @@ import { Field } from 'react-final-form';
 const getkey = (name, index) => `radio::${name}::${index}`;
 
 const RadioGroup = ({
-  name, label, provider, ...rest
-}) => (
-  <p>
-    <label htmlFor={name}>
-      <span>{label}</span>
-      {provider &&
-        provider.map((obj, index) => (
-          <span key={getkey(name, index)}>
-            <Field {...rest} name={name} type="radio" component="input" />
-            <span>{obj.name}</span>
-          </span>
-        ))}
-    </label>
-  </p>
-);
+  name, label, provider, display, ...rest
+}) => {
+  const cssclass = `flex-${display !== 'inline' ? 'rows' : 'columns'}`;
+  return (
+    <p>
+      <label htmlFor={name}>
+        <span>{label}</span>
+        <span className={cssclass}>
+          {provider &&
+            provider.map((obj, index) => (
+              <span key={getkey(name, index)}>
+                <Field {...rest} name={name} type="radio" component="input" />
+                <span>{obj.name}</span>
+              </span>
+            ))}
+        </span>
+      </label>
+    </p>
+  );
+};
 
 RadioGroup.propTypes = {
   name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  display: PropTypes.string.isRequired,
   provider: PropTypes.array.isRequired,
 };
 
