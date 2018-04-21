abf43d4bba9f29a8cf8d4525952a3044b9510043
sixertoy <sixertoy.github@gmail.com>
2018-04-21 08:53:02 +0200

    🐛  🗄  ajout d'un apollo link pour ne plus envoyer __typename dans les requetes du de l'admin

--- admin/src/apollo.js
+++ admin/src/apollo.js
@@ -4,6 +4,25 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createNetworkStatusNotifier } from 'react-apollo-network-status';
 
+// FIXME ->
+// prevents __typename to be send to server
+// github.com/apollographql/apollo-client/issues/1564#issuecomment-357492659
+const createOmitTypenameLink = () => {
+  const omitTypename = (key, value) =>
+    (key === '__typename' ? undefined : value);
+
+  return new ApolloLink((operation, forward) => {
+    if (operation.variables) {
+      // eslint-disable-next-line
+      operation.variables = JSON.parse(
+        JSON.stringify(operation.variables),
+        omitTypename,
+      );
+    }
+    return forward(operation);
+  });
+};
+
 const {
   NetworkStatusNotifier,
   link: networkStatusNotifierLink,
@@ -18,7 +37,10 @@ export const createClient = (uri) => {
   const client = new ApolloClient({
     cache: new InMemoryCache(),
     connectToDevTools: process.env.NODE_ENV !== 'production',
-    link: ApolloLink.from([networkStatusNotifierLink.concat(httpLink)]),
+    link: ApolloLink.from([
+      createOmitTypenameLink(),
+      networkStatusNotifierLink.concat(httpLink),
+    ]),
   });
   return { client, NetworkStatusNotifier };
 };
