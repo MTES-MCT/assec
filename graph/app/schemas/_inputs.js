3252da28cb1038e1c7f5d07f12b47a5512dbafb6
sixertoy <sixertoy.github@gmail.com>
2018-04-24 21:57:30 +0200

    🗑  [GRAPH] cleanup graphql inputs

--- graph/app/schemas/_inputs.js
+++ graph/app/schemas/_inputs.js
@@ -15,24 +15,6 @@ input AlerteInput {
   end_date: String
   start_date: String
 }
-
-input ZoneInput {
-  dpt: ID!
-  help: String
-  label: String!
-  order: String!
-  geojson: String
-  alerte: [AlerteInput]
-}
-
-input DepartementInput {
-  id: ID
-  code: String!
-  label: String!
-  usages: [SUOInput]!
-  origines: [SUOInput]!
-  situations: [SUOInput]!
-}
 `;
 
 export default inputsTypeDefs;
