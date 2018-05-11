03173a21f7a6390aea257e28addba871001e95be
sixertoy <sixertoy.github@gmail.com>
2018-05-11 14:29:03 +0200

    🚧 [WIDGET] ajout de la vue markdown pour la description d'une restriction

--- widget/src/app/components/FormResults.js
+++ widget/src/app/components/FormResults.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactMarkdown from 'react-markdown';
 
 const FormResults = ({ rules }) => (
   <div id="form-results" className="flex-rows">
     {rules.map(obj => (
       <div key={`rule::${obj.id}`}>
         <h3>{obj.title}</h3>
-        <p>{obj.description}</p>
+        <ReactMarkdown className="markdown-body" source={obj.description} />
       </div>
     ))}
   </div>
