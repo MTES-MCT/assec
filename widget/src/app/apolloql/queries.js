f00b97cb4244caba17a19b8757ea79c7df138136
sixertoy <sixertoy.github@gmail.com>
2018-05-16 10:16:16 +0200

    🚧 [WIDGET] fix du type dans la query pour obtenir la restriction

--- widget/src/app/apolloql/queries.js
+++ widget/src/app/apolloql/queries.js
@@ -2,8 +2,8 @@ import gql from 'graphql-tag';
 
 export const LOAD_RESTRICTION_CASE = gql(`
 query findRestriction (
-  $usages: String!
-  $origines: String!
+  $usages: ID!
+  $origines: ID!
   $situations: [Float]!
 ) {
   findRestriction (
