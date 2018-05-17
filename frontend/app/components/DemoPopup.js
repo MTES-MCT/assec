4031352e6d6d7c4a63ab5ca546137a4b5ab10bbd
sixertoy <sixertoy.github@gmail.com>
2018-05-17 15:31:25 +0200

    🚧 [WIDGET] ajout du bouton pour fermer la popup

--- frontend/app/components/DemoPopup.js
+++ frontend/app/components/DemoPopup.js
@@ -1,26 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 // application
 import getConfig from 'next/config';
+import { closePopin } from './../actions';
 
 const { publicRuntimeConfig: envconfig } = getConfig();
 const uri = envconfig.widgeturi;
 
-const DemoPopup = () => (
+const DemoPopup = ({ dispatch }) => (
   <div id="demo-popup">
-    <React.Fragment>
-      <div className="overlay" style={{ opacity: 1 }} />
-      <div className="container" style={{ opacity: 1 }}>
-        <div className="inner">
-          <iframe title="assec-demo-widget"
-            width="100%"
-            height="100%"
-            frameBorder="0"
-            src={`${uri}?department=83`} />
-        </div>
+    <div className="overlay" style={{ opacity: 1 }} />
+    <div className="container" style={{ opacity: 1 }}>
+      <button type="button"
+        className="close-popup-button"
+        onClick={() => dispatch(closePopin())}>
+        <i className="icon icon-cancel" />
+        <span>Fermer</span>
+      </button>
+      <div className="inner">
+        <iframe title="assec-demo-widget"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          src={`${uri}?department=83`} />
       </div>
-    </React.Fragment>
+    </div>
   </div>
 );
 
-export default DemoPopup;
+DemoPopup.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(DemoPopup);
