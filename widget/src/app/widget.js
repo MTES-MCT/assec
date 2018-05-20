cd084db5b004b93cecf8b505ecca5349371e353f
sixertoy <sixertoy.github@gmail.com>
2018-05-20 09:29:44 +0200

    📱 [WIDGET] ajout des classes css responsive pour mobile/tablet

--- widget/src/app/widget.js
+++ widget/src/app/widget.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
+import withSizes from 'react-sizes';
 // import { Query } from 'react-apollo';
 import { connect } from 'react-redux';
 import queryString from 'query-string';
@@ -68,14 +69,18 @@ class Widget extends React.Component {
 
   render () {
     const { code } = this.state;
-    const { step, popin, welcome } = this.props;
+    const {
+      step, popin, welcome, ismobile, istablet,
+    } = this.props;
     if (!code) return renderError(404);
     const stepclass = `current-step-${step}`;
     const popinclass = `${((welcome || popin) && 'haspopin') || ''}`;
+    const responsiveclass =
+      (ismobile && 'mobile') || (istablet && 'tablet') || '';
     return (
       <React.Fragment>
         <Helmet>
-          <body className={`${stepclass} ${popinclass}`} />
+          <body className={`${stepclass} ${popinclass} ${responsiveclass}`} />
           <title>Assec{usedebug() ? ' | Development' : ''}</title>
         </Helmet>
         <div id="assec-widget" className="flex-rows flex-between p20">
@@ -162,12 +167,19 @@ Widget.defaultProps = {
 Widget.propTypes = {
   // form: PropTypes.object,
   step: PropTypes.number.isRequired,
+  ismobile: PropTypes.bool.isRequired,
+  istablet: PropTypes.bool.isRequired,
   welcome: PropTypes.oneOfType([PropTypes.bool]).isRequired,
   popin: PropTypes.oneOfType([PropTypes.bool, PropTypes.object]).isRequired,
 };
 
-export default connect(state => ({
+const connected = connect(state => ({
   step: state.step,
   popin: state.popin,
   welcome: state.welcome,
 }))(Widget);
+
+export default withSizes(({ width }) => ({
+  ismobile: width < 480,
+  istablet: width >= 480 && width < 1024,
+}))(connected);
