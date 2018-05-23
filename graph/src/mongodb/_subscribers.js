0624daef0b183b72fc8592260cb0ea1669829b9a
sixertoy <sixertoy.github@gmail.com>
2018-05-23 10:55:16 +0200

    🗄  [GRAPH] ajout des props preferences pour un subscriber

--- graph/src/mongodb/_subscribers.js
+++ graph/src/mongodb/_subscribers.js
@@ -1,5 +1,30 @@
 import Mongoose, { Schema } from 'mongoose';
 
+const LatLngSchema = new Schema({
+  lat: { type: String, required: true },
+  lng: { type: String, required: true },
+});
+
+const PreferencesSchema = new Schema({
+  department: {
+    required: true,
+    ref: 'departments',
+    type: Schema.Types.ObjectId,
+  },
+  origines: {
+    required: true,
+    type: Schema.Types.ObjectId,
+  },
+  usages: {
+    required: true,
+    type: Schema.Types.ObjectId,
+  },
+  latlng: {
+    required: true,
+    type: LatLngSchema,
+  },
+});
+
 const SubscriberSchema = new Schema(
   {
     email: {
@@ -10,6 +35,10 @@ const SubscriberSchema = new Schema(
       required: true,
       ref: 'departments',
       type: Schema.Types.ObjectId,
+    },
+    preferences: {
+      required: false,
+      type: [{ type: PreferencesSchema }],
     },
   },
   {
